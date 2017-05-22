b4d79470e911931f7b2b082e3123335e8d2766b7
altonelli <altonelli14@gmail.com>
2017-05-22 15:31:09 -0700

    created stack with 2 queues

---
+++ problems/QueueWithTwoStacks.java
@@ -0,0 +1,61 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class QueueWithTwoStacks {
+
+    private static class MyQueue {
+        Stack<Integer> stackOne;
+        Stack<Integer> stackTwo;
+
+        public MyQueue() {
+            this.stackOne = new Stack<Integer>();
+            this.stackTwo = new Stack<Integer>();
+        }
+
+        public void add(int num) {
+            stackOne.push(num);
+        }
+
+        public Integer poll() {
+            if (stackTwo.isEmpty()) {
+                while (!stackOne.isEmpty())
+                    stackTwo.push(stackOne.pop());
+            }
+            return stackTwo.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Queue<Integer> stdQueue = new LinkedList<Integer>();
+        MyQueue myQueue = new MyQueue();
+        Stack<Integer> stdStack = new Stack<Integer>();
+
+        for (int i = 1; i < 10; i++) {
+            stdQueue.add(i);
+            myQueue.add(i);
+            stdStack.add(i);
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(stdQueue.poll() + " ");
+            System.out.print(myQueue.poll() + " ");
+            System.out.println(stdStack.pop());
+        }
+
+        for (int i = 5; i <= 10; i++) {
+            stdQueue.add(i);
+            myQueue.add(i);
+            stdStack.add(i);
+        }
+
+        while (!stdQueue.isEmpty()) {
+            System.out.print(stdQueue.poll() + " ");
+            System.out.print(myQueue.poll() + " ");
+            System.out.println(stdStack.pop());
+        }
+
+    }
+}
+
